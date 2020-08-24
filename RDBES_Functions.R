cfb87e74ffd60eff60bdfe2de450c901f0f5cd59
David Currie <davidcurrie2001@yahoo.co.uk>
2020-08-24 17:39:52 +0100

    Further work on test data functions

--- RDBES_Functions.R
+++ RDBES_Functions.R
@@ -2800,9 +2800,12 @@ createNewTestDataRow <- function(HierarchyToGenerate,LowerHierarchyToGenerate, T
       # SPECIAL COLUMN - DEhierarchyCorrect
     } else if (myColName == 'DEhierarchyCorrect') {
       myNewValue <- 'Y'
-      # SPECIAL COLUMN - DEhierarchyCorrect
+      # SPECIAL COLUMN - SAlowerHierarchy
     } else if (myColName == 'SAlowerHierarchy') {
       myNewValue <- LowerHierarchyToGenerate
+      # SPECIAL COLUMN - BVfishID
+    } else if (myColName == 'BVfishId') {
+      myNewValue <- RowID
       # SPECIAL COLUMN - XXid
     } else if (myColName == paste(TableType,'id',sep="")) {
       myNewValue <- RowID
@@ -2923,4 +2926,313 @@ createNewTestDataRow <- function(HierarchyToGenerate,LowerHierarchyToGenerate, T
   myNewRowValues
   
 }
+
+#' makeTestDataMoreRealistic
+#'
+#' @param DataToUse 
+#' @param CountryToUse 
+#' @param YearToUse 
+#' @param MetierList 
+#' @param SpeciesList 
+#' @param RDBEScodeLists 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+makeTestDataMoreRealistic <- function(DataToUse,CountryToUse,YearToUse,MetierList,SpeciesList,RDBEScodeLists){
+  
+  # For testing
+  #DataToUse <- myTestData
+  #RDBEScodeLists <- allowedValues
+  #CountryToUse <- 'IE'
+  #SpeciesList <- c(126436)
+  #MetierList <- c('OTB_DEF_100-119_0_0')
+  
+  # VESSEL DETAILS
+  DataToUse[['VD']][,'VDcountry'] <- CountryToUse
+  DataToUse[['VD']][,'VDflagCountry'] <- CountryToUse
+  DataToUse[['VD']][,'VDyear'] <- YearToUse
+  # Lets pick some random vessel lengths
+  myVesselLengths <- RDBEScodeLists[RDBEScodeLists$listName == 'tRS_VesselLengthCategory','Key']
+  DataToUse[['VD']][,'VDlengthCategory'] <- sample(myVesselLengths,nrow(DataToUse[['VD']]),replace = TRUE)
+  DataToUse[['VD']][,'VDencryptedVesselCode'] <- paste('VDcode_',DataToUse[['VD']][,'VDid'],sep="")
+  
+  # SPECIES LIST DETAILS
+  DataToUse[['SL']][,'SLcountry'] <- CountryToUse
+  DataToUse[['SL']][,'SLyear'] <- YearToUse
+  DataToUse[['SL']][,'SLspeciesListName'] <- paste(CountryToUse,'_',YearToUse,'_SpeciesList',sep="")
+  mySpeciesCodes <- RDBEScodeLists[RDBEScodeLists$listName == 'tSpecWoRMS','Key']
+  mySpeciesCodes <- c(SpeciesList,mySpeciesCodes)
+  mySpeciesCodes <- unique(mySpeciesCodes)
+  mySpeciesCodes <- mySpeciesCodes[1:nrow(DataToUse[['SL']])]
+  DataToUse[['SL']][,'SLcommercialTaxon'] <- mySpeciesCodes
+  DataToUse[['SL']][,'SLspeciesCode'] <- mySpeciesCodes
+  
+  # CS TABLES
+  
+  # COUNTRY
+  # Sort out the country and location code fields
+  DataToUse[['SD']][,'SDcountry'] <- CountryToUse
+  if ('LE' %in% names(DataToUse)){
+    DataToUse[['LE']][,'LEcountry'] <- CountryToUse
+  }
+  
+  # LOCODE
+  # For fields that use locodes we'll pick a random locode from the country we are interested in
+  myCountryLocodes <- RDBEScodeLists[RDBEScodeLists$listName == 'tHarbour_LOCODE','Key']
+  myCountryLocodes <- myCountryLocodes[grepl(paste('^',CountryToUse,'.*$',sep=''), myCountryLocodes)]
+  if ('FT' %in% names(DataToUse)){
+    if (length(myCountryLocodes) > 1){
+      myRandomValues <- sample(myCountryLocodes,nrow(DataToUse[['FT']]),replace = TRUE)
+    } else {
+      myRandomValues <- myCountryLocodes
+    }
+    DataToUse[['FT']][,'FTdepartureLocation'] <- myRandomValues
+    DataToUse[['FT']][,'FTarrivalLocation'] <- myRandomValues
+  }
+  if ('OS' %in% names(DataToUse)){
+    if (length(myCountryLocodes) > 1){
+      myRandomValues <- sample(myCountryLocodes,nrow(DataToUse[['OS']]),replace = TRUE)
+    } else {
+      myRandomValues <- myCountryLocodes
+    }
+    DataToUse[['OS']][,'OSlocode'] <- myRandomValues
+  }
+  if ('LE' %in% names(DataToUse)){
+    if (length(myCountryLocodes) > 1){
+      myRandomValues <- sample(myCountryLocodes,nrow(DataToUse[['LE']]),replace = TRUE)
+    } else {
+      myRandomValues <- myCountryLocodes
+    }
+    DataToUse[['LE']][,'LElocation'] <- myRandomValues
+  }
+  if ('LO' %in% names(DataToUse)){
+    if (length(myCountryLocodes) > 1){
+      myRandomValues <- sample(myCountryLocodes,nrow(DataToUse[['LO']]),replace = TRUE)
+    } else {
+      myRandomValues <- myCountryLocodes
+    }
+    DataToUse[['LO']][,'LOlocode'] <- myRandomValues
+  }
+  
+  # YEAR
+  DataToUse[['DE']][,'DEyear'] <- YearToUse
+  
+  # DATES
+  # For dates we'll pick a date within the year we are using
+  if ('FT' %in% names(DataToUse)){
+    myRandomValues <- sample(seq(as.Date(paste(YearToUse,'/01/01',sep="")), as.Date(paste(YearToUse,'/12/01',sep="")), by="day"), nrow(DataToUse[['FT']]),replace = TRUE)
+    myRandomTripLength <- sample(1:30,nrow(DataToUse[['FT']]),replace = TRUE)
+    
+    DataToUse[['FT']][,'FTdepartureDate'] <- format(myRandomValues,'%Y-%m-%d')
+    DataToUse[['FT']][,'FTarrivalDate'] <- format(myRandomValues + myRandomTripLength,'%Y-%m-%d')
+  }
+  # for FO we'll use the arrival date of the trip for all our fishing acitvity
+  if ('FO' %in% names(DataToUse) & 'FT' %in% names(DataToUse)){
+    FTDates <- DataToUse[['FT']][,c('FTid','FTdepartureDate','FTarrivalDate')]
+    myJoin <- inner_join(DataToUse[['FO']],FTDates, by ='FTid')
+    DataToUse[['FO']][,'FOendDate'] <- myJoin[,'FTarrivalDate']
+  }
+  if ('OS' %in% names(DataToUse)){
+    myRandomValues <- sample(seq(as.Date(paste(YearToUse,'/01/01',sep="")), as.Date(paste(YearToUse,'/12/01',sep="")), by="day"), nrow(DataToUse[['OS']]),replace = TRUE)
+    DataToUse[['OS']][,'OSsamplingDate'] <- format(myRandomValues,'%Y/%m/%d')
+  }
+  if ('LE' %in% names(DataToUse)){
+    myRandomValues <- sample(seq(as.Date(paste(YearToUse,'/01/01',sep="")), as.Date(paste(YearToUse,'/12/01',sep="")), by="day"), nrow(DataToUse[['LE']]),replace = TRUE)
+    DataToUse[['LE']][,'LEdate'] <- format(myRandomValues,'%Y/%m/%d')
+  }
+  
+  # SPECIES LIST NAME
+  DataToUse[['SS']][,'SSspeciesListName'] <- paste(CountryToUse,'_',YearToUse,'_SpeciesList',sep="")
+  
+  # VESSELS
+  # Ensure we are only referring to vessel that appear in our Vessel Details - this gets a bit complex because we can have the encryptedVesselCode used in a few tables, which can be linked to each other
+  myVesselCodes <- DataToUse[['VD']][,'VDencryptedVesselCode']
+  
+  # If we have VS data - pick a random vessel for each VS row, and then ensure that any FT or LE rows connected to it use the same value
+  if ('VS' %in% names(DataToUse)){
+    
+    if (length(myVesselCodes) > 1){
+      myRandomValues <- sample(myVesselCodes,nrow(DataToUse[['VS']]),replace = TRUE)
+    } else {
+      myRandomValues <- myVesselCodes
+    }
+    
+    DataToUse[['VS']][,'VSencryptedVesselCode'] <- myRandomValues
+    DataToUse[['VS']][,'VSunitName'] <- myRandomValues
+  }
+  
+  # IF we have FT data we need to see whether it is connected to an VS record - if so, use the same vessel code, else pick a random one
+  if ('FT' %in% names(DataToUse)){
+    # Clear out any existing values first
+    DataToUse[['FT']][,'FTencryptedVesselCode'] <- NA
+    
+    # If we have a VS table then we'll use the vessel code form that record in the FT record
+    if ('VS' %in% names(DataToUse)){
+      VSvessel <- DataToUse[['VS']][,c('VSid','VSencryptedVesselCode')]
+      myJoin <- inner_join(DataToUse[['FT']],VSvessel, by ='VSid')
+      DataToUse[['FT']][,'FTencryptedVesselCode'] <- myJoin[,'VSencryptedVesselCode']
+    } 
+    
+    # Now use random vessels for any remaining NA vessel codes
+    if (length(myVesselCodes) > 1){
+      myRandomValues <- sample(myVesselCodes,nrow(DataToUse[['FT']][is.na(DataToUse[['FT']][,'FTencryptedVesselCode']),]),replace = TRUE)
+    } else {
+      myRandomValues <- myVesselCodes
+    }
+    if (nrow(DataToUse[['FT']][is.na(DataToUse[['FT']][,'FTencryptedVesselCode']),])>0){
+      DataToUse[['FT']][is.na(DataToUse[['FT']][,'FTencryptedVesselCode']),'FTencryptedVesselCode'] <- myRandomValues
+    }
+    
+  }
+  
+  # If we have LE data it can be directly linked to either a VS or FT record - need to use the same vessel code if it is - else pick a random one
+  if ('LE' %in% names(DataToUse)){
+    
+    # Clear out any existing values first
+    DataToUse[['LE']][,'LEencryptedVesselCode'] <- NA
+    
+    # If we have a VS table then we'll use the vessel code form that record in the FT record
+    if ('VS' %in% names(DataToUse)){
+      VSvessel <- DataToUse[['VS']][,c('VSid','VSencryptedVesselCode')]
+      myJoin <- inner_join(DataToUse[['LE']],VSvessel, by ='VSid')
+      DataToUse[['LE']][,'LEencryptedVesselCode'] <- myJoin[,'VSencryptedVesselCode']
+    } 
+    
+    # If we have a FT table then we'll use the vessel code form that record in the FT record
+    if ('FT' %in% names(DataToUse)){
+      FTvessel <- DataToUse[['FT']][,c('FTid','FTencryptedVesselCode')]
+      myJoin <- inner_join(DataToUse[['LE']],FTvessel, by ='FTid')
+      DataToUse[['LE']][,'LEencryptedVesselCode'] <- myJoin[,'FTencryptedVesselCode']
+    } 
+    
+    # Now use random vessels for any remaining NA vessel codes
+    if (length(myVesselCodes) > 1){
+      myRandomValues <- sample(myVesselCodes,nrow(DataToUse[['LE']][is.na(DataToUse[['LE']][,'LEencryptedVesselCode']),]),replace = TRUE)
+    } else {
+      myRandomValues <- myVesselCodes
+    }
+    if (nrow(DataToUse[['LE']][is.na(DataToUse[['LE']][,'LEencryptedVesselCode']),])>0){
+      DataToUse[['LE']][is.na(DataToUse[['LE']][,'LEencryptedVesselCode']),'LEencryptedVesselCode'] <- myRandomValues
+    }
+    
+  }
+  
+  # SPECIES
+  
+  # Ensure we are only sampling species that appear in our Species Details
+  mySpeciesCodes <- DataToUse[['SL']][,'SLspeciesCode']
+  if (length(mySpeciesCodes) > 1){
+    myRandomValues <- sample(mySpeciesCodes,nrow(DataToUse[['SA']]),replace = TRUE)
+  } else {
+    myRandomValues <- mySpeciesCodes
+  }
+  DataToUse[['SA']][,'SAspeciesCode'] <- myRandomValues
+  
+  # METIERS
+  
+  # If we have been given a list of metiers then we'll just use them 
+  if (!is.null(MetierList)){
+    myMetiersCodes <- unique(MetierList)
+    # Otherwise we'll generate a list of metiers from the code list
+  } else {
+    myMetiersCodes <- RDBEScodeLists[RDBEScodeLists$listName == 'tMetier6_FishingActivity','Key']
+    # Pick the maximum number of metiers for our list based on whatever is the biggest number from i) the number of SA records, ii) the number 1 (to cover the very rare case when we don't have any samples)
+    numerOfMetiersToUse <- max(nrow(DataToUse[['SA']]), 1 )
+    myMetiersCodes <- myMetiersCodes[1:numerOfMetiersToUse]
+  }
+  
+  # If we have FO data pick a random metier and gear code
+  if ('FO' %in% names(DataToUse)){
+    # use random metier
+    if (length(myMetiersCodes) > 1){
+      myRandomValues <- sample(myMetiersCodes,nrow(DataToUse[['FO']]),replace = TRUE)
+    } else {
+      myRandomValues <- myMetiersCodes
+    }
+    DataToUse[['FO']][,'FOmetier6'] <- myRandomValues
+    DataToUse[['FO']][,'FOgear'] <- substring(myRandomValues,1,3)
+  }
+  
+  # If we have LE data pick a random metier and gear code
+  if ('LE' %in% names(DataToUse)){
+    # use random metier
+    if (length(myMetiersCodes) > 1){
+      myRandomValues <- sample(myMetiersCodes,nrow(DataToUse[['LE']]),replace = TRUE)
+    } else {
+      myRandomValues <- myMetiersCodes
+    }
+    DataToUse[['LE']][,'LEmetier6'] <- myRandomValues
+    DataToUse[['LE']][,'LEgear'] <- substring(myRandomValues,1,3)
+  }
+  
+  # If we have SA data pick a random metier and gear code
+  if ('SA' %in% names(DataToUse)){
+    # use random metier
+    if (length(myMetiersCodes) > 1){
+      myRandomValues <- sample(myMetiersCodes,nrow(DataToUse[['SA']]),replace = TRUE)
+    } else {
+      myRandomValues <- myMetiersCodes
+    }
+    DataToUse[['SA']][,'SAmetier6'] <- myRandomValues
+    DataToUse[['SA']][,'SAgear'] <- substring(myRandomValues,1,3)
+    
+    # Ensure SAsequence number is unique
+    DataToUse[['SA']][,'SAsequenceNumber'] <- DataToUse[['SA']][,'SAid']
+  }
+  
+  # FREQUENCY MEASURE
+  
+  if ('FM' %in% names(DataToUse)){
+    
+    # For each species sampled we'll randomly pick a minimum length and then create soem length data in the FM data
+    speciesSampled <- unique(DataToUse[['SA']][,'SAspeciesCode'])
+    minLength <- data.frame( SAspeciesCode = speciesSampled, minLength = sample(10:40,length(speciesSampled), replace = TRUE), stringsAsFactors = FALSE)
+    myJoin <- inner_join(DataToUse[['SA']],minLength, by ='SAspeciesCode')
+    myJoin2 <- inner_join(DataToUse[['FM']],myJoin,by='SAid')
+    DataToUse[['FM']][,'FMclass'] <- myJoin2[,'minLength'] * 10
+    
+    # For each sample, create increasing values for FMclass
+    # TODO - shoudl be a better way to do this
+    for (mySAid in unique(DataToUse[['FM']][,'SAid'])){
+      myFMDataForASample <- DataToUse[['FM']][DataToUse[['FM']][,'SAid'] == mySAid,]
+      currentLength <- NA
+      for (myFMid in myFMDataForASample[,'FMid']){
+        if (is.na(currentLength)){
+          currentLength <- myFMDataForASample[myFMDataForASample[,'FMid']==myFMid,'FMclass']
+        } else {
+          currentLength <- currentLength + 10
+        }
+        # Add 10mm to previous length
+        myFMDataForASample[myFMDataForASample[,'FMid'] == myFMid,'FMclass'] <- currentLength
+        # Make a random number of fish at that length
+        myFMDataForASample[myFMDataForASample[,'FMid'] == myFMid,'FMnumberAtUnit'] <- sample(1:10,1)
+        
+      }
+      DataToUse[['FM']][DataToUse[['FM']][,'SAid'] == mySAid,] <- myFMDataForASample
+    }
+  }
+  
+  # BIOLOGICAL VARIABLES
+  if ('BV' %in% names(DataToUse)){
+    
+    # Sort out BVfishId so that we don't have duplicates
+    DataToUse[['BV']][,'BVfishId'] <- DataToUse[['BV']][,'BVid']
+    # Pick a random age
+    myBVages <- DataToUse[['BV']][DataToUse[['BV']][,'BVtype'] == 'Age',]
+    if (nrow(myBVages)>0){
+      DataToUse[['BV']][DataToUse[['BV']][,'BVtype'] == 'Age','BVvalue'] <- as.character(sample(1:10, nrow(myBVages),replace = TRUE))
+      DataToUse[['BV']][DataToUse[['BV']][,'BVtype'] == 'Age','BVvalueType'] <- 'Year'
+    }
+    
+    
+  }
+  
+  
+  # Return our data
+  DataToUse
+  
+}
+
 

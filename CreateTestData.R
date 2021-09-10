3821ae17b686a26324b7c500260d16306d05c574
David Currie <davidcurrie2001@yahoo.co.uk>
2021-09-10 17:09:58 +0100

    Can create CL and CE test data as well now

--- CreateTestData.R
+++ CreateTestData.R
@@ -25,6 +25,10 @@ allowedValues <- loadReferenceData(downloadFromICES = FALSE)
 allRequiredTables <- getTablesInHierarchies(downloadFromGitHub = FALSE, fileLocation = './tableDefs/')
 #allRequiredTables <- getTablesInHierarchies(downloadFromGitHub = TRUE, fileLocation = './tableDefs/')
 
+# 8/9/21 temp fix - need to check XSD files are up-to-date
+allRequiredTables[["H12"]]<- c("DE","SD","LO","TE","LE","SS","SA","FM","BV")
+allRequiredTables[["H13"]]<- c("DE","SD","FO","SS","SA","FM","BV")
+
 ## STEP 2) GENERATE TEST DATA
 
 # Can use a loop to generate test data for all hierarchies if you want to 
@@ -33,27 +37,37 @@ for (i in 1:13){
   
   # Define some parameters for our test data
   #myHierarchyToGenerate <- 'H1'
+
   print(myHierarchyToGenerate)
   myLowerHierarchyToGenerate <- 'A'
-  myYear <- 2000
+  myYear <- 1965
   myCountry <- 'ZW'
   # Number of strata in different tables - if no value if given for a table then it is assumed to be unstratified
-  myStrata <- list(DE = 2, VS = 2)
+  myStrata <- list(DE = 3, VS = 3, FT = 3, OS = 3, LO = 1, TE = 1)
   # Number of things sampled in different tables - if no value is given for a table then it is assumed to be 1
-  mySampled <- list(VS=3,FO=3,SS=1,SA=3, FM=10,BV=2, VD=10, SL=20)
+  mySampled <- list(VS=3,FO=3,SS=1,SA=2, FM=10,BV=2, VD=10, SL=20, OS = 3, TE = 3, LO = 3, FT=3, CE=100, CL = 500)
   # Total number of things in different tables - if no value is given for a table then it is assumed to be equal to the number sampled + 1
-  myTotal <- list(VS=30,FO=10,SS=4,FM=10,BV=2)
+  myTotal <- list(VS=30,FO=10,SS=4,FM=10,BV=2, OS = 100, LO = 100, TE = 10, FT = 100)
   # Select methods used in different tables - if no value is given for a table then it is assumed to be simple random sampling SRSWR
   myMethods <- list()
   
   # Generate some random data
   myTestData <- createTestData(HierarchyToGenerate = myHierarchyToGenerate, LowerHierarchyToGenerate = myLowerHierarchyToGenerate, RDBESvalidationdata = validationData, RDBEScodeLists = allowedValues, RequiredTables = allRequiredTables, NumberOfStrata = myStrata, NumberSampled = mySampled, NumberTotal = myTotal, SelectionMethods = myMethods)
   
-  # The data we just geenerated is too random and won't pass validation or upload check - lets fix that now
+  # The data we just generated is too random and won't pass validation or upload check - lets fix that now
   myNewTestData <- makeTestDataMoreRealistic(DataToUse = myTestData,CountryToUse=myCountry,YearToUse=myYear,MetierList= NULL,SpeciesList= NULL,RDBEScodeLists=allowedValues)
   
+  # Sampling scheme name for WGRDBES-EST
+  myNewTestData[['DE']]$DEsamplingScheme <- 'WGRDBES-EST TEST 1'
+  
   # Lets validate our data (Optional)
-  #errorsTestData <- validateTables(RDBESdata = myNewTestData, RDBESvalidationdata = validationData, RDBEScodeLists = allowedValues, shortOutput = TRUE,framestoValidate = c("BV","DE","FM","FO","FT","LE","LO","OS","SA","SD","SL","SS","TE","VD","VS"))
+  #errorsTestData <- validateTables(RDBESdata = myNewTestData, RDBESvalidationdata = validationData, RDBEScodeLists = allowedValues, shortOutput = TRUE,framestoValidate = c("BV","DE","FM","FO","FT","LE","LO","OS","SA","SD","SL","SS","TE","VD","VS","CE","CL"))
+  
+  # Create a CE output file
+  generateSimpleExchangeFile(typeOfFile = 'CE', yearToUse = myYear, country = myCountry, RDBESdata = myNewTestData,cleanData = TRUE, RDBESvalidationdata = validationData, RDBEScodeLists = allowedValues)
+  
+  # Create a CL output file
+  generateSimpleExchangeFile(typeOfFile = 'CL', yearToUse = myYear, country = myCountry, RDBESdata = myNewTestData,cleanData = TRUE, RDBESvalidationdata = validationData, RDBEScodeLists = allowedValues)
   
   # Create a VD output file
   generateSimpleExchangeFile(typeOfFile = 'VD', yearToUse = myYear, country = myCountry, RDBESdata = myNewTestData,cleanData = TRUE, RDBESvalidationdata = validationData, RDBEScodeLists = allowedValues)
